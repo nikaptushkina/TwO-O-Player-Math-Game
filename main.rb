3e52b34c616a1b7c151f6803518700f245807c10
Rachel PR <rplourder@gmail.com>
2022-07-20 12:24:00 -0300

    Add in start for a new game

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
-#contains all the rb files
+#contains all the rb files and starts a new game
 
 require './players.rb'
 require './questions.rb'
 require './game.rb'
+
+new_game = Game.new("New Game")
+new_game.start
