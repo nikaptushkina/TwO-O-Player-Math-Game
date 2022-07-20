3ebc7aabc645890d9d4785d16f5eaff42b5a2918
Rachel PR <rplourder@gmail.com>
2022-07-20 12:22:54 -0300

    Split out question and answer methods

--- questions.rb
+++ questions.rb
@@ -1,17 +1,21 @@
-# Chooses two numbers between 1 - 20 to make addition provlems
+# Chooses two numbers between 1 - 20 to make addition problems
 # Check the answer
 
 class Question
   attr_reader :newQuestion, :answer
 
   def initialize
-    num1 = rand(1..20)
-    num2 = rand(1..20)
-    @newQuestion = "What does #{num1} plus #{num2} equal?"
-    @answer = num1 + num2
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
+  end
+
+
+  def ask_question(name)
+    puts "#{name}: What does #{@num1} plus #{@num2} equal?"
+  end
+
+  def check_answer?(input)
+    @sum == input
   end
 end
-
-q = Question.new
-puts q.newQuestion
-puts q.answer
